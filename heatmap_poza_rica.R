81a0c559759956665fa8c3976e64f807eea5591f
fdzul <felipe.dzul.m@gmail.com>
2025-10-15 18:14:08 -0600

    This commit update the dash

---
+++ heatmap_poza_rica.R
@@ -0,0 +1,29 @@
+
+
+
+densnv::mp_heatmap(geocoded_dataset = z,
+                   cve_edo = "30",
+                   locality = "Poza Rica de Hidalgo",
+                   status_caso = c(1, 2),
+                   kernel = 000,
+                   week = c(1:53),
+                   zoom = NULL,
+                   map_type = NULL,
+                   alpha = 0.6,
+                   static = FALSE,
+                   save = FALSE,
+                   palette = viridis::turbo)
+
+
+densnv::mp_heatmap(geocoded_dataset = z,
+                   cve_edo = "30",
+                   locality = "Tuxpan de Rodriguez Cano",
+                   status_caso = c(1, 2),
+                   kernel = 000,
+                   week = c(1:53),
+                   zoom = NULL,
+                   map_type = NULL,
+                   alpha = 0.6,
+                   static = FALSE,
+                   save = FALSE,
+                   palette = viridis::turbo)
